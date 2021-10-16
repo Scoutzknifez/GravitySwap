1f045cd5d3762d6cf81a30b45f054c4a0dcf9892
FamousHero <33110962+FamousHero@users.noreply.github.com>
2021-10-15 17:08:54 -0700

    Client-side scripts

--- Assets/Scripts/ServerClient/MyClient.cs
+++ Assets/Scripts/ServerClient/MyClient.cs
@@ -15,6 +15,8 @@ public class MyClient : MonoBehaviour
     public int myId;
     public TCP tcp;
 
+    private delegate void PacketHandler(Packet _packet);
+    private static Dictionary<int, PacketHandler> packetHandlers;
     private void Awake()
     {
         if (instance == null)
@@ -35,12 +37,15 @@ public class MyClient : MonoBehaviour
 
     public void ConnectToServer()
     {
+        InitializeClientData();
         tcp.Connect();
     }
+
     public class TCP
     {
         public TcpClient socket;
         private NetworkStream stream;
+        private Packet receivedData;
         private byte[] receiveBuffer;
 
         public void Connect()
@@ -54,6 +59,20 @@ public class MyClient : MonoBehaviour
             socket.BeginConnect(instance.ip, instance.port, ConnectCallback, socket);
 
         }
+        public void SendData(Packet _packet)
+        {
+            try
+            {
+                if(socket != null)
+                {
+                    stream.BeginWrite(_packet.ToArray(),  0, _packet.Length(), null, null);
+                }
+            }
+            catch(Exception _ex)
+            {
+                Debug.Log($"Error sending data to server via TCP: {_ex}");
+            }
+        }
 
         private void ConnectCallback(IAsyncResult _result)
         {
@@ -65,7 +84,7 @@ public class MyClient : MonoBehaviour
             }
 
             stream = socket.GetStream();
-
+            receivedData = new Packet();
             stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
         }
 
@@ -80,7 +99,7 @@ public class MyClient : MonoBehaviour
                 }
                 byte[] _data = new byte[_byteLength];
                 Array.Copy(receiveBuffer, _data, _byteLength);
-
+                receivedData.Reset(HandleData(_data));
                 stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
             }
             catch (Exception _ex)
@@ -89,5 +108,57 @@ public class MyClient : MonoBehaviour
 
             }
         }
+
+        private bool HandleData(byte[] _data)
+        {
+            int _packetLength = 0; 
+            receivedData.SetBytes(_data);
+
+            if(receivedData.UnreadLength() >= 4)
+            {
+                _packetLength = receivedData.ReadInt();
+                if(_packetLength <= 0)
+                {
+                    return true;
+                }
+            }
+
+            while(_packetLength > 0 && _packetLength <= receivedData.UnreadLength())
+            {
+                byte[] _packetBytes = receivedData.ReadBytes(_packetLength);
+                ThreadManager.ExecuteOnMainThread(() =>
+                {
+                    using(Packet _packet = new Packet(_packetBytes))
+                    {
+                        int _packetId = _packet.ReadInt();
+                        packetHandlers[_packetId](_packet);
+                    }
+                });
+
+                _packetLength = 0;
+                if(receivedData.UnreadLength() >= 4)
+                {
+                    _packetLength = receivedData.ReadInt();
+                    if(_packetLength <= 0)
+                    {
+                        return true;
+                    }
+                }
+            }
+            if(_packetLength <= 1)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    private void InitializeClientData()
+    {
+        packetHandlers = new Dictionary<int, PacketHandler>()
+        {
+            {(int)ServerPackets.welcome, ClientHandle.Welcome}
+        };
+        Debug.Log("Initialize packets.");
     }
 }
